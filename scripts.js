05ae1244d2ff700f7b0f94be8d7e5798c6c79f65
Seb Bains <sebastianbains@gmail.com>
2020-06-10 16:41:51 +0100

    completed project functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
 const background = document.getElementById("background");
+let newColor = "";
+let previousColor = "";
 const preSelectedColors=["red","blue","green","red"];
 const colorButton = document.getElementById("flipButton");
-flipButton.addEventListener("click",updateBackground());
+flipButton.addEventListener("click",updateBackground, false);
 
 function updateBackground(){
-    let newColor = preSelectedColors[Math.floor(Math.random()*4)];
-    console.log(newColor);
+    getNewColor();
     background.style.background= newColor;
-}
+    console.log(newColor);
+    console.log(previousColor);
+    previousColor = newColor;
+};
+
+function getNewColor(){
+    newColor = preSelectedColors[Math.floor(Math.random()*4)];
+    if(newColor===previousColor){
+        getNewColor();
+    };
+};
